25031ead7e51e4487269aa0144c073ef7e319ed8
Roman Kraizman <roman.krayzman@gmail.com>
2022-10-04 00:46:32 +0300

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const whatsYourName = () => {
-    var userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
+  let userName = readlineSync.question('May I have your name? ');
+  console.log('Hello, ' + userName + '!');
 };
 
 export default whatsYourName;
