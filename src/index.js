9ce80d76b9526fa7c42ab77e6f1d0f2102e79a14
Roman Kraizman <roman.krayzman@gmail.com>
2022-10-07 23:25:32 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,49 +1,27 @@
 import readlineSync from 'readline-sync';
-import randomIntFromInterval from './randomIntFromInterval.js';
 
-// Common logic for all tasks
-const whatsYourName = () => {
+const commonGameEngine = (task, game) => {
   console.log('Welcome to the Brain Games!');
+
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  return userName;
-};
 
-const congrats = () => {
-  const userName = whatsYourName();
+  console.log(task);
+
+  for (let i = 0; i < 3; i += 1) {
+    const questionAndResult = game();
+    console.log('Question: ', questionAndResult[0]);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const rightAnswer = questionAndResult[1];
+    if (userAnswer === rightAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer}". \nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+
   console.log(`Congratulations, ${userName}!`);
 };
 
-// Logic for brain-even game
-const brainEven = () => {
-  const userName = whatsYourName();
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  let roundCount = 0;
-  while (roundCount < 3) {
-    const rndInt = randomIntFromInterval(6, 35);
-    console.log(`Question: ${rndInt}`);
-    const answer = readlineSync.prompt({ prompt: 'Your answer: ' });
-    if (answer === 'yes' && rndInt % 2 === 0) {
-      console.log('Correct!');
-      roundCount += 1;
-    } else if (answer === 'no' && rndInt % 2 !== 0) {
-      console.log('Correct!');
-      roundCount += 1;
-    } else if (answer === 'yes' && rndInt % 2 !== 0) {
-      return console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
-    } else if (answer === 'no' && rndInt % 2 === 0) {
-      return console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
-    } else if (answer !== 'yes' && answer !== 'no') {
-      return console.log(`Let's try again, ${userName}!`);
-    }
-  }
-  return congrats;
-};
-
-// Logic for brain-calc game
-const brainCalc = () => {
-  whatsYourName();
-};
-
-export { whatsYourName, brainEven, brainCalc };
+export default commonGameEngine;
