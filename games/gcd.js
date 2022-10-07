2f16c1a6372096cb5bda57c5997c6afcc0df6d71
Roman Kraizman <roman.krayzman@gmail.com>
2022-10-08 00:51:06 +0300

    add gcd.js

---
+++ games/gcd.js
@@ -0,0 +1,28 @@
+import commonGameEngine from '../src/index.js';
+import randomIntFromInterval from '../src/randomIntFromInterval.js';
+
+const gcd = () => {
+  const taskForCalc = 'Find the greatest common divisor of given numbers.';
+
+  const gameEngineForGcd = () => {
+    let rndNum1 = randomIntFromInterval(1, 100);
+    let rndNum2 = randomIntFromInterval(1, 100);
+
+    const question = `${rndNum1} ${rndNum2}`;
+
+    let result = '';
+    while (rndNum1 !== 0 && rndNum2 !== 0) {
+      if (rndNum1 > rndNum2) {
+        rndNum1 %= rndNum2;
+      } else {
+        rndNum2 %= rndNum1;
+      }
+      result = rndNum1 + rndNum2;
+    }
+    return [question, (result).toString()];
+  };
+
+  commonGameEngine(taskForCalc, gameEngineForGcd);
+};
+
+export default gcd;
