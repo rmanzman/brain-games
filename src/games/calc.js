ecc6cf7d49872e3e48d4922fc89cc69d5c620cd4
Roman Kraizman <roman.krayzman@gmail.com>
2022-10-16 15:37:28 +0300

    update calc.js

---
+++ src/games/calc.js
@@ -0,0 +1,41 @@
+import gameEngine from '../index.js';
+import rndNumFromInt from '../helper.js';
+
+const description = 'What is the result of the expression?';
+
+const calculate = (rndNum1, rndNum2, rndMathSign) => {
+  let result = 0;
+
+  switch (rndMathSign) {
+    case '+':
+      result = rndNum1 + rndNum2;
+      break;
+    case '-':
+      result = rndNum1 - rndNum2;
+      break;
+    case '*':
+      result = rndNum1 * rndNum2;
+      break;
+    default:
+      result = null;
+  }
+  return String(result);
+};
+
+const questionAndAnswer = () => {
+  const rndNum1 = rndNumFromInt(1, 10);
+  const rndNum2 = rndNumFromInt(1, 30);
+
+  const mathSign = ['+', '-', '*'];
+  const rndMathSign = mathSign[rndNumFromInt(0, 2)];
+
+  const question = `${rndNum1} ${rndMathSign} ${rndNum2}`;
+
+  const expectedAnswer = calculate(rndNum1, rndNum2, rndMathSign);
+
+  return [question, expectedAnswer];
+};
+
+export default () => {
+  gameEngine(description, questionAndAnswer);
+};
