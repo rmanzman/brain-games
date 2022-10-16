5bc18bd966730920c5501b62d044b9408d96fdd0
Roman Kraizman <roman.krayzman@gmail.com>
2022-10-16 15:37:48 +0300

    update gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,28 @@
+import gameEngine from '../index.js';
+import rndNumFromInt from '../helper.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (rndNum1, rndNum2) => {
+  for (let i = rndNum1; i > 0; i -= 1) {
+    if (rndNum1 % i === 0 && rndNum2 % i === 0) {
+      return String(i);
+    }
+  }
+  return undefined;
+};
+
+const questionAndAnswer = () => {
+  const rndNum1 = rndNumFromInt(1, 100);
+  const rndNum2 = rndNumFromInt(1, 100);
+
+  const question = `${rndNum1} ${rndNum2}`;
+
+  const expectedAnswer = getGcd(rndNum1, rndNum2);
+
+  return [question, expectedAnswer];
+};
+
+export default () => {
+  gameEngine(description, questionAndAnswer);
+};
