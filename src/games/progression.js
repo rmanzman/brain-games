8ebc6e663dbbe8fbabdcf220ef44f9331409799f
Roman Kraizman <roman.krayzman@gmail.com>
2022-10-16 15:38:11 +0300

    update progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,33 @@
+import gameEngine from '../index.js';
+import rndNumFromInt from '../helper.js';
+
+const description = 'What number is missing in the progression?';
+
+const getProgression = () => {
+  const lengthOfProg = rndNumFromInt(10, 20);
+  const firstNumOfProg = rndNumFromInt(1, 10);
+  const stepOfProg = rndNumFromInt(1, 5);
+
+  const arr = [firstNumOfProg];
+
+  for (let i = 0; arr.length <= lengthOfProg; i += 1) {
+    arr.push(arr[i] + stepOfProg);
+  }
+
+  return arr;
+};
+
+const questionAndAnswer = () => {
+  const progression = getProgression();
+  const indexForDots = rndNumFromInt(0, 10);
+
+  const expectedAnswer = String(progression[indexForDots]);
+  progression[indexForDots] = '..';
+  const question = progression.join(' ');
+
+  return [question, expectedAnswer];
+};
+
+export default () => {
+  gameEngine(description, questionAndAnswer);
+};
