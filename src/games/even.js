9459de2e600aabf5246f3ca9bb61372b1962b440
Roman Kraizman <roman.krayzman@gmail.com>
2022-10-16 15:37:38 +0300

    update even.js

---
+++ src/games/even.js
@@ -0,0 +1,20 @@
+import gameEngine from '../index.js';
+import rndNumFromInt from '../helper.js';
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (rndNum) => rndNum % 2 === 0;
+
+const questionAndAnswer = () => {
+  const rndNum = rndNumFromInt(1, 50);
+
+  const question = `${rndNum}`;
+
+  const expectedAnswer = isEven(rndNum) ? 'yes' : 'no';
+
+  return [question, expectedAnswer];
+};
+
+export default () => {
+  gameEngine(description, questionAndAnswer);
+};
